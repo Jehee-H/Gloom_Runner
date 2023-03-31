04d599da427776d98ff63470ec70c546215eeb7a
Jehee-H <jehee.han1701@gmail.com>
2023-03-31 03:47:28 +0200

    recovery 6

--- Gloom_Runner/Assets/Scripts/SWEP UnsereScripts/Collectables.cs
+++ Gloom_Runner/Assets/Scripts/SWEP UnsereScripts/Collectables.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class Collectables : MonoBehaviour
 {
     private CoinScoreManager coinScoreManager;
+    private AudioSource source;
+    public AudioClip ring;
 
     private void Start()
     {
         coinScoreManager = GameObject.Find("UI").GetComponent<CoinScoreManager>();
+        source = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Coin"))
         {
+
+            source.PlayOneShot(ring);
+
             Debug.Log("IchWarHier");
 
             coinScoreManager.coinScore += 1f;
--- Gloom_Runner/Assets/Scripts/SWEP UnsereScripts/EnemyHit.cs
+++ Gloom_Runner/Assets/Scripts/SWEP UnsereScripts/EnemyHit.cs
@@ -7,17 +7,21 @@ public class EnemyHit : MonoBehaviour
 
     public GameObject gameController;
     Platformer.UI.MetaGameController metaGameController;
+    private AudioSource source;
+    public AudioClip deadSound;
 
 
     void Awake()
     {
         metaGameController = gameController.GetComponent<Platformer.UI.MetaGameController>();
+        source = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
+            source.PlayOneShot(deadSound);
             metaGameController.ToggleDefeatMenu(true);
         }
     }
